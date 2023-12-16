dab5d086f5482c87d01b2eb0b2dd653f962eaf9e
nick.du <nickdu2009@gmail.com>
2023-12-16 17:36:22 +0800

    add trace

--- middleware/request-id.go
+++ middleware/request-id.go
@@ -3,11 +3,15 @@ package middleware
 import (
 	"context"
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/otel"
 	"one-api/common"
 )
 
 func RequestId() func(c *gin.Context) {
 	return func(c *gin.Context) {
+		tracer := otel.Tracer("one-api/controller/request-id")
+		ctx, span := tracer.Start(c.Request.Context(), "RequestId")
+		defer span.End()
 		id := common.GetTimeString() + common.GetRandomString(8)
 		c.Set(common.RequestIdKey, id)
 		ctx := context.WithValue(c.Request.Context(), common.RequestIdKey, id)
